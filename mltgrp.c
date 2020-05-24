b67f893062fcc9276bdd3c590f44fac5f2a52a8f
Gianpietro Signorini <gianpietros@aol.com>
2020-05-24 18:30:10 +0100

    Multiple graph plot to compare countries new daily cases. Can now select between 2 and 5 countries and will plot results on one graph. Legend will display showing country name.

--- mltgrp.c
+++ mltgrp.c
@@ -36,10 +36,14 @@ void compareGraph(int *arrayDate, int *arraytcases, int numrec, char countryname
   temp = fopen(fileSpec, "w");
   
   //FILE * gnuplotPipe = popen("gnuplot -persistent 2> /dev/null", "w"); 
+
+  fprintf(temp, "%s\n", countryname);
+  fclose(temp);
+  temp = fopen(fileSpec, "a");
   
   for (i=0; i < numrec; i++)
     {
-      fprintf(temp, "%d %d \n", arrayDate[i], arraytcases[i]); //Write the data to a temporary file
+      fprintf(temp, "%d %d\n", arrayDate[i], arraytcases[i]); //Write the data to a temporary file
     }
   /*
   fprintf(gnuplotPipe, "set title 'NEW CASES - %s'\n", countryname);
@@ -62,10 +66,10 @@ void buildGraph(int g) {
    strcpy(commandsForGnuplot[0],"set xtics border out rotate by 90 offset character 0, -2, 0 autojustify");
    strcpy(commandsForGnuplot[1],"set xtics font ',5'");
    strcpy(commandsForGnuplot[2],"set ytics font ',6'");   
-   strcpy(commandsForGnuplot[3],"set ylabel 'Total number of Cases");
+   strcpy(commandsForGnuplot[3],"set ylabel 'Number of New Cases");
    strcpy(commandsForGnuplot[4],"set xlabel 'Date'");
    strcpy(commandsForGnuplot[5],"set grid");
-   strcpy(commandsForGnuplot[6],"unset key");     
+   strcpy(commandsForGnuplot[6],"set key autotitle columnhead");     
    if (g == 2){
    strcpy(commandsForGnuplot[7],"plot for [i=1:2] 'country'.i.'.gp' using 1:2:xtic(1) with lines");
    } else if ( g == 3){
@@ -78,7 +82,7 @@ void buildGraph(int g) {
 
   FILE * gnuplotPipe = popen("gnuplot -persistent 2> /dev/null", "w"); 
 
-  //  fprintf(gnuplotPipe, "set title 'NEW CASES' \n");
+  fprintf(gnuplotPipe, "set title 'NEW CASES' \n");
   
   for (i=0; i < numOfCommands; i++) {
        fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
